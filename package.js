832212bbb22f3ef9c2a0beea916cfd7ce56fa01e
Ilshidur <nicolas-coutin@outlook.com>
2016-07-24 14:23:04 +0200

    First commit

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: 'ilshidur:zoom-vanilla',
+  version: '0.0.1',
+  summary: 'Medium\'s Image Zoom without jQuery. https://spinningarrow.github.io/zoom-vanilla.js wrapped for Meteor.',
+  git: 'https://github.com/Ilshidur/meteor-zoom-vanilla'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    'lib/zoom-vanilla.js/dist/zoom-vanilla.min.js',
+    'lib/zoom-vanilla.js/css/zoom.css'
+  ], 'client');
+});
